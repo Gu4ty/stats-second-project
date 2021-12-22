6e411ee677f227be1a2ba3624e6351918977d102
RodroVMS <democlesvanshepars@gmail.com>
2021-12-22 16:21:06 -0500

    Format comments

--- src/reduction_techniques/pca.R
+++ src/reduction_techniques/pca.R
@@ -7,7 +7,8 @@ employee[, names] <- lapply(employee[, names], factor)
 
 glimpse(employee)
 
-# Convirtiendo las variables categoricas en variables binarias. Codigo extraido y adaptado de
+# Convirtiendo las variables categoricas en variables binarias.
+# Codigo extraido y adaptado de:
 # https://stackoverflow.com/questions/33990760/converting-factors-to-binary-in-r
 setDT(employee)[, c(levels(employee$workclass), "workclass") :=
     c(lapply(levels(workclass), function(x) as.integer(x == workclass)), .(NULL))] # nolint
@@ -33,8 +34,8 @@ setDT(employee)[, c(levels(employee$income), "income") :=
 setDT(employee)[, c(levels(employee$sex), "sex") :=
     c(lapply(levels(sex), function(x) as.integer(x == sex)), .(NULL))] # nolint
 
-# Se elimina native.country debido a que a tiene muy mala distribucion y que no aporta realmente
-# nada importa excepto que 37 variables adicionales
+# Se elimina native.country debido a que a tiene muy mala distribucion y que
+# no aporta realmente nada importa excepto que 37 variables adicionales
 
 # setDT(employee)[, c(levels(employee$native.country), "native.country") :=
 # c(lapply(levels(native.country), function(x) as.integer(x == native.country)), .(NULL))] # nolint
@@ -44,61 +45,14 @@ acp <- prcomp(employee, scale. = T)
 summary(acp)
 plot(acp)
 
-# Esta tecnica es realmente mala para este set de datos. Es necesario expandir las variables categoricas
-# a variables binarias, lo que implica un incremento de 107 varialbe hecho ingenuamente. Si se realiza con
-# tiempo se puede reducir este numero a la mitad, aun asi representa un aumento considerable de variables
-# con respecto a los datos originales.
-# Ademas sobre las variables extraidas como no se tiene correalcion entre los datos las componentes son muy
-# poco representativas de la poblacion, luego se necesitan una gran cantida.
-# Luego esta tecnica implica un incremento de dimension y un decremento de certeza.
+# Esta tecnica es realmente mala para este set de datos. Es necesario expandir
+# las variables categoricas a variables binarias, lo que implica un incremento
+# de 107 varialbe hecho ingenuamente. Si se realiza con tiempo se puede reducir
+# este numero a la mitad, aun asi representa un aumento considerable de
+# variables con respecto a los datos originales.
+# Ademas sobre las variables extraidas como no se tiene correalcion entre los
+# datos las componentes son muy poco representativas de la poblacion, luego se
+# necesitan una gran cantidad. Luego esta tecnica implica un incremento de
+# dimension y un decremento de certeza.
 
 acp$rotation
-
-
-
-# =============================================================================
-# https://stackoverflow.com/questions/33990760/converting-factors-to-binary-in-r
-library(dplyr)
-library(tidyr)
-
-df <- data.frame(a = c(1:3), b = c(1:3), y = c("Rose", "Pink", "Pink"), d = c(1:3)) # nolint
-df$y <- as.factor(df$y)
-
-cbind(df[1:2], sapply(levels(df$y), function(x) as.integer(x == df$y)), df[4])
-
-# This one works
-# apply to every one of them and do some pca
-setDT(df)[, c(levels(df$y), "y") :=
-    c(lapply(levels(y), function(x) as.integer(x == y)), .(NULL))]
-
-
-# other solution
-df %>%
-    mutate(
-        value = 1,
-        x = paste0("is", x)
-    ) %>%
-    spread(x, value, fill = 0)
-
-b <- employee %>%
-    mutate(
-        value = 1,
-        marital.status = paste0("is", marital.status)
-    ) %>%
-    pivot_wider(
-        names_from = marital.status,
-        values_from = value,
-        values_fill = 0
-    )
-library(tidyverse)
-
-b <- df %>%
-    mutate(
-        value = 1,
-        y = paste0("Is", y)
-    ) %>%
-    pivot_wider(
-        names_from = y,
-        values_from = value,
-        values_fill = 0
-    )
