b40290c3b55439972485bad27943fb24917cb529
RodroVMS <democlesvanshepars@gmail.com>
2021-12-22 19:30:06 -0500

    Improve data visualization

--- src/reduction_techniques/kmeans.R
+++ src/reduction_techniques/kmeans.R
@@ -7,6 +7,40 @@ library(caret)
 # se multiplicaria hasta los 100
 library(clustMixType)
 
+# Adaptacion de la funcion clprofiles de la libreria clustMixType,
+# se afecta principalment el barplot para mostrar la leyenda entera
+clprofiles <- function(object, x, col = NULL) {
+    vars <- 1:ncol(x)
+    clusids <- sort(unique(object$cluster))
+    if (length(col) != max(clusids)) {
+          warning("Length of col should match number of clusters!")
+      }
+    par(ask = TRUE)
+    for (i in vars) {
+        if (is.numeric(x[, i])) {
+            boxplot(x[, i] ~ object$cluster, col = col, main = colnames(x)[i])
+            legend("topright", legend = clusids, fill = col)
+        }
+        if (is.factor(x[, i])) {
+            tab <- table(x[, i], object$cluster)
+            for (j in 1:length(object$size)) {
+                tab[, j] <- tab[
+                    ,
+                    j
+                ] / object$size[j]
+            }
+            barplot(t(tab),
+                beside = TRUE, main = colnames(x)[i],
+                col = col,
+                las = 2,
+                cex.names = 0.7
+            )
+        }
+    }
+    par(ask = FALSE)
+    invisible()
+}
+
 
 # Cargando, eliminando filas con valores NA y estandarizando
 employee <- read.csv("../raw_data/adult.data.csv")
@@ -21,6 +55,13 @@ pre_proc_val <- preProcess(employee[, cols], method = c("center", "scale"))
 # Aplicando la normalizcion a al set de entrenamiento y prueba
 employee[, cols] <- predict(pre_proc_val, employee[, cols])
 
+# Eliminando capital loss y capital gain, son datos muy sesgados. Si se
+# observa su boxplot se ve que que son mayoritariamente puntos aberrantes
+# Pues la mayoria de los datos se concentran  en 0
+boxplot(employee$capital.gain)
+boxplot(employee$capital.loss)
+employee <- subset(employee, select = c(-capital.loss, -capital.gain))
+
 
 # Utilizando una semilla fija para obtener resultados deterministas
 set.seed(1)
@@ -31,8 +72,18 @@ a <- validation_kproto(method = "mcclain", data = employee, k = 2:5)
 
 # Intuimos que tener dos clusters es buena pues muestra grupos
 # bien definidos
-employee <- subset(employee, select = c(-capital.loss))
-fit <- kproto(employee, 4, verbose = T)
 
-plot(employee, col = fit$cluster)
-clprofiles(fit, employee)
+# Aplicando kproto
+k <- 4
+fit <- kproto(employee, k, verbose = T)
+
+cluster_colors <- c()
+colors <- c("#ffcc66", "#ff99ff", "#99ff99", "#66ccff", "#996666", "#6666ff")
+for (i in 1:length(fit$cluster)) {
+    cluster_colors <- append(cluster_colors, colors[fit$cluster[i]])
+}
+
+plot(employee, col = cluster_colors)
+clprofiles(fit, employee, col = colors[1:k])
+
+
